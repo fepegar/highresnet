2f8c2333928f929347ea192e214d53230739115b
Fernando Perez-Garcia <fepegar@gmail.com>
2019-07-15 11:25:54 +0100

    Rearrange classes

--- highresnet/highresnet.py
+++ highresnet/highresnet.py
@@ -10,167 +10,6 @@ PADDING_MODES = {
 }
 
 
-class Pad3d(nn.Module):
-    def __init__(self, pad, mode):
-        assert mode in PADDING_MODES.keys()
-        super().__init__()
-        self.pad = 6 * [pad]
-        self.mode = mode
-
-    def forward(self, x):
-        return F.pad(x, self.pad, self.mode)
-
-
-class ConvolutionalBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, dilation,
-                 dimensions, batch_norm=True, instance_norm=False,
-                 padding_mode='constant', preactivation=True, kernel_size=3,
-                 activation=True):
-        assert padding_mode in PADDING_MODES.keys()
-        assert not (batch_norm and instance_norm)
-        super().__init__()
-
-        if dimensions == 2:
-            padding_class = getattr(nn, f'{PADDING_MODES[padding_mode]}Pad2d')
-            padding_instance = padding_class(dilation)
-        elif dimensions == 3:
-            padding_instance = Pad3d(dilation, padding_mode)
-        conv_class = nn.Conv2d if dimensions == 2 else nn.Conv3d
-
-        if batch_norm:
-            norm_class = nn.BatchNorm2d if dimensions == 2 else nn.BatchNorm3d
-        if instance_norm:
-            norm_class = nn.InstanceNorm2d if dimensions == 2 else nn.InstanceNorm3d
-
-        layers = nn.ModuleList()
-
-        if preactivation:
-            if batch_norm or instance_norm:
-                layers.append(norm_class(in_channels))
-            if activation:
-                layers.append(nn.ReLU())
-
-        if kernel_size > 1:
-            layers.append(padding_instance)
-
-        conv_layer = conv_class(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            dilation=dilation,
-            bias=False,
-        )
-        layers.append(conv_layer)
-
-        if not preactivation:
-            if batch_norm or instance_norm:
-                layers.append(norm_class(out_channels))
-            if activation:
-                layers.append(nn.ReLU())
-
-        self.convolutional_block = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.convolutional_block(x)
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, num_layers, dilation, dimensions,
-                 batch_norm=True, instance_norm=False, residual=True,
-                 residual_type='pad'):
-        assert residual_type in ('pad', 'project')
-        super().__init__()
-        self.residual = residual
-        self.change_dimension = in_channels != out_channels
-        self.residual_type = residual_type
-        self.dimensions = dimensions
-        if self.change_dimension:
-            if residual_type == 'project':
-                conv_class = nn.Conv2d if dimensions == 2 else nn.Conv3d
-                self.change_dim_layer = conv_class(
-                    in_channels,
-                    out_channels,
-                    kernel_size=1,
-                    dilation=dilation,
-                    bias=False,
-                )
-
-        conv_blocks = nn.ModuleList()
-        for _ in range(num_layers):
-            conv_block = ConvolutionalBlock(
-                in_channels,
-                out_channels,
-                dilation,
-                dimensions,
-                batch_norm=batch_norm,
-                instance_norm=instance_norm,
-            )
-            conv_blocks.append(conv_block)
-            in_channels = out_channels
-        self.residual_block = nn.Sequential(*conv_blocks)
-
-    def forward(self, x):
-        """
-        From the original ResNet paper, page 4:
-
-        "When the dimensions increase, we consider two options:
-
-        (A) The shortcut still performs identity mapping,
-        with extra zero entries padded for increasing dimensions.
-        This option introduces no extra parameter
-
-        (B) The projection shortcut in Eqn.(2) is used to
-        match dimensions (done by 1×1 convolutions).
-
-        For both options, when the shortcuts go across feature maps of
-        two sizes, they are performed with a stride of 2."
-        """
-        out = self.residual_block(x)
-        if self.residual:
-            if self.change_dimension:
-                if self.residual_type == 'project':
-                    x = self.change_dim_layer(x)
-                elif self.residual_type == 'pad':
-                    if self.dimensions == 2:
-                        N, _, H, W = x.shape
-                        diff = out.shape[1] - x.shape[1]  # diff of channels
-                        zeros = x.new_zeros(N, diff, H, W)
-                    elif self.dimensions == 3:
-                        N, _, D, H, W = x.shape
-                        diff = out.shape[1] - x.shape[1]  # diff of channels
-                        zeros_half = x.new_zeros(N, diff // 2, D, H, W)
-                    x = torch.cat((zeros_half, x, zeros_half), dim=1)  # channels dimension
-            out = x + out
-        return out
-
-
-class DilationBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, dilation, dimensions,
-                 layers_per_block=2, num_residual_blocks=3, batch_norm=True,
-                 instance_norm=False, residual=True):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        residual_blocks = nn.ModuleList()
-        for _ in range(num_residual_blocks):
-            residual_block = ResidualBlock(
-                in_channels,
-                out_channels,
-                layers_per_block,
-                dilation,
-                dimensions,
-                batch_norm=batch_norm,
-                instance_norm=instance_norm,
-                residual=residual,
-            )
-            residual_blocks.append(residual_block)
-            in_channels = out_channels
-        self.dilation_block = nn.Sequential(*residual_blocks)
-
-    def forward(self, x):
-        return self.dilation_block(x)
-
-
 class HighResNet(nn.Module):
     def __init__(self,
                  in_channels,
@@ -187,7 +26,7 @@ class HighResNet(nn.Module):
                  padding_mode='constant',
                  add_dropout_layer=False,
                  add_last_norm_layer=True,
-        ):
+                 ):
         assert padding_mode in PADDING_MODES.keys()
         assert dimensions in (2, 3)
         super().__init__()
@@ -237,7 +76,7 @@ class HighResNet(nn.Module):
         # Add dropout layer as in NiftyNet
         if add_dropout_layer:
             in_channels = out_channels
-            out_channels = 80  ####
+            out_channels = 80
             dropout_conv_block = ConvolutionalBlock(
                 in_channels=in_channels,
                 out_channels=out_channels,
@@ -306,3 +145,165 @@ class HighRes3DNet(HighResNet):
     def __init__(self, *args, **kwargs):
         kwargs['dimensions'] = 3
         super().__init__(*args, **kwargs)
+
+
+class DilationBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, dilation, dimensions,
+                 layers_per_block=2, num_residual_blocks=3, batch_norm=True,
+                 instance_norm=False, residual=True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        residual_blocks = nn.ModuleList()
+        for _ in range(num_residual_blocks):
+            residual_block = ResidualBlock(
+                in_channels,
+                out_channels,
+                layers_per_block,
+                dilation,
+                dimensions,
+                batch_norm=batch_norm,
+                instance_norm=instance_norm,
+                residual=residual,
+            )
+            residual_blocks.append(residual_block)
+            in_channels = out_channels
+        self.dilation_block = nn.Sequential(*residual_blocks)
+
+    def forward(self, x):
+        return self.dilation_block(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, num_layers, dilation, dimensions,
+                 batch_norm=True, instance_norm=False, residual=True,
+                 residual_type='pad'):
+        assert residual_type in ('pad', 'project')
+        super().__init__()
+        self.residual = residual
+        self.change_dimension = in_channels != out_channels
+        self.residual_type = residual_type
+        self.dimensions = dimensions
+        if self.change_dimension:
+            if residual_type == 'project':
+                conv_class = nn.Conv2d if dimensions == 2 else nn.Conv3d
+                self.change_dim_layer = conv_class(
+                    in_channels,
+                    out_channels,
+                    kernel_size=1,
+                    dilation=dilation,
+                    bias=False,
+                )
+
+        conv_blocks = nn.ModuleList()
+        for _ in range(num_layers):
+            conv_block = ConvolutionalBlock(
+                in_channels,
+                out_channels,
+                dilation,
+                dimensions,
+                batch_norm=batch_norm,
+                instance_norm=instance_norm,
+            )
+            conv_blocks.append(conv_block)
+            in_channels = out_channels
+        self.residual_block = nn.Sequential(*conv_blocks)
+
+    def forward(self, x):
+        """
+        From the original ResNet paper, page 4:
+
+        "When the dimensions increase, we consider two options:
+
+        (A) The shortcut still performs identity mapping,
+        with extra zero entries padded for increasing dimensions.
+        This option introduces no extra parameter
+
+        (B) The projection shortcut in Eqn.(2) is used to
+        match dimensions (done by 1×1 convolutions).
+
+        For both options, when the shortcuts go across feature maps of
+        two sizes, they are performed with a stride of 2."
+        """
+        out = self.residual_block(x)
+        if self.residual:
+            if self.change_dimension:
+                if self.residual_type == 'project':
+                    x = self.change_dim_layer(x)
+                elif self.residual_type == 'pad':
+                    if self.dimensions == 2:
+                        N, _, H, W = x.shape
+                        diff = out.shape[1] - x.shape[1]  # diff of channels
+                        zeros = x.new_zeros(N, diff, H, W)
+                    elif self.dimensions == 3:
+                        N, _, D, H, W = x.shape
+                        diff = out.shape[1] - x.shape[1]  # diff of channels
+                        zeros_half = x.new_zeros(N, diff // 2, D, H, W)
+                    x = torch.cat((zeros_half, x, zeros_half),
+                                  dim=1)  # channels dimension
+            out = x + out
+        return out
+
+
+class ConvolutionalBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, dilation,
+                 dimensions, batch_norm=True, instance_norm=False,
+                 padding_mode='constant', preactivation=True, kernel_size=3,
+                 activation=True):
+        assert padding_mode in PADDING_MODES.keys()
+        assert not (batch_norm and instance_norm)
+        super().__init__()
+
+        if dimensions == 2:
+            padding_class = getattr(nn, f'{PADDING_MODES[padding_mode]}Pad2d')
+            padding_instance = padding_class(dilation)
+        elif dimensions == 3:
+            padding_instance = Pad3d(dilation, padding_mode)
+        conv_class = nn.Conv2d if dimensions == 2 else nn.Conv3d
+
+        if batch_norm:
+            norm_class = nn.BatchNorm2d if dimensions == 2 else nn.BatchNorm3d
+        if instance_norm:
+            norm_class = nn.InstanceNorm2d if dimensions == 2 else nn.InstanceNorm3d
+
+        layers = nn.ModuleList()
+
+        if preactivation:
+            if batch_norm or instance_norm:
+                layers.append(norm_class(in_channels))
+            if activation:
+                layers.append(nn.ReLU())
+
+        if kernel_size > 1:
+            layers.append(padding_instance)
+
+        conv_layer = conv_class(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            dilation=dilation,
+            bias=False,
+        )
+        layers.append(conv_layer)
+
+        if not preactivation:
+            if batch_norm or instance_norm:
+                layers.append(norm_class(out_channels))
+            if activation:
+                layers.append(nn.ReLU())
+
+        self.convolutional_block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.convolutional_block(x)
+
+
+class Pad3d(nn.Module):
+    def __init__(self, pad, mode):
+        assert mode in PADDING_MODES.keys()
+        super().__init__()
+        self.pad = 6 * [pad]
+        self.mode = mode
+
+    def forward(self, x):
+        return F.pad(x, self.pad, self.mode)
