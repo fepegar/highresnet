9d509e2e1169f62f6fe1aef7edec4d050088fa05
Fernando Perez-Garcia <fepegar@gmail.com>
2019-07-16 09:36:32 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='highresnet',
     version='0.1.0',
-    packages=[
-        'highresnet',
+    author='Fernando Perez-Garcia',
+    author_email='fernando.perezgarcia.17@ucl.ac.uk',
+    description='PyTorch implementation of HighResNet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fepegar/highresnet',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
