9d449b9d9ecceb691a3fae864a03232f3eb7773c
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-07-18 09:34:03 +0100

    Add docstring

--- hubconf.py
+++ hubconf.py
@@ -4,6 +4,9 @@ dependencies = ['torch']
 
 
 def highres3dnet(*args, pretrained=False, **kwargs):
+    """
+    HighRes3DNet by Li et al. 2017 for T1-MRI brain parcellation
+    """
     from highresnet import HighRes3DNet
     model = HighRes3DNet(*args, **kwargs)
     if pretrained:
