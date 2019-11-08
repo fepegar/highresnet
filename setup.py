868a129617a5621121bf3a145c1316bf24a5d541
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-08 17:01:44 +0000

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/highresnet',
-    version='0.9.0',
+    version='0.9.1',
     zip_safe=False,
 )
