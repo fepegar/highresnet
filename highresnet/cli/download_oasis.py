4e31ec7021487e651d21d5a7e6f6be75a98a7561
Fernando Pérez-García <fepegar@gmail.com>
2020-01-25 17:48:38 +0000

    Update download_oasis.py

--- highresnet/cli/download_oasis.py
+++ highresnet/cli/download_oasis.py
@@ -11,8 +11,7 @@ import tarfile
 import click
 
 
-@click.command()
-def main():
+def download_oasis():
     """
     http://blog.ppkt.eu/2014/06/python-urllib-and-tarfile/
     """
@@ -26,7 +25,12 @@ def main():
     nii_filepath = os.path.join(dir_name, nii_filename)
     os.rename(nii_filepath, nii_filename)
     shutil.rmtree(dir_name)
-    print(nii_filename)
+    return nii_filename
+
+
+@click.command()
+def main():
+    print(download_oasis())
 
 
 if __name__ == "__main__":
