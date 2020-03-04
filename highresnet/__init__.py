a6e5741813d8101fd99a60e56695a243dcc06ff1
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2020-03-04 20:06:18 +0000

    Bump version: 0.10.1 → 0.10.2

--- highresnet/__init__.py
+++ highresnet/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Fernando Perez-Garcia"""
 __email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
-__version__ = '0.10.1'
+__version__ = '0.10.2'
 
 import sys
 INSTALL = '\nInstall it by running:\npip install "torch>=1.1"'
